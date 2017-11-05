b374ec0a999c60322ea0eef7e508f9dde160aa25
Drilon <drilonavdiu@gmail.com>
2017-11-05 20:53:08 +0100

    Committing with HelloUser class added.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.out.println("Hello ASE2017 - How are you? :)");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("What's your name?");
+		String userName=scanner.nextLine();
+		scanner.close();
+		HelloUser helloUser = new HelloUser(userName);
+		helloUser.greetUser();
+		
 	}
 
 }
