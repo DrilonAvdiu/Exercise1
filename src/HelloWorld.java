3dd129efb85f9f054e8949944858f18ac7c0ea90
Drilon <drilonavdiu@gmail.com>
2017-11-05 22:00:30 +0100

    Fixing conflicts (2)

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Hello ASE2017 - how are you now?)");
+		// TODO Auto-generated method stub
+		System.out.println("Hello ASE2017 - how are you now? :)");
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("What's your name?");
 		String userName=scanner.nextLine();
